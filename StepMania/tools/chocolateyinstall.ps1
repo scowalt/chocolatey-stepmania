c6ea647054225fa0920c0b60b3f451a0579421a9
Scott Walters <me@scowalt.com>
2017-03-19 16:39:12 -0700

    5.0.12 (#1)
    * 5.0.12
    
    * Removed comments from install and uninstall files

---
+++ StepMania/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'StepMania'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/stepmania/stepmania/releases/download/v5.0.12/StepMania-5.0.12-win32.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'StepMania*'
+
+  checksum      = 'B3A6C5F70CC35FA5A756E489B545D495309B2ABDC5EFAA7A78ABBACDC2AA8F2C'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
